ee0f49966ab94e8a454bfe0971d14dc376f909a3
surace007 <suracerijal.007@gmail.com>
2024-11-23 20:52:15 +0545

    calendar using jagged array

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -10,21 +10,29 @@ namespace Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first Number : ");
-            int firstNumber = Int32.Parse(Console.ReadLine());
+            //Program to print calendar using jagged arrray
 
-            Console.WriteLine("Enter the second Number : ");
-            int secondNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Printing the calendar using jagged array (one month).\n\n");
+            //initializing jagged array
+            int[][] calendar =
+            {
+                new int[] {1,2,3,4,5,6,7},
+                new int[] {8,9,10,11,12,13,14},
+                new int[] {15,16,17,18,19,20,21 },
+                new int[] {22,23,24,25,26,27,28},
+                new int[] {29,30,31,32}
 
-            Console.WriteLine("Before Swapping:-");
-            Console.WriteLine($"First Number = {firstNumber}  , Second Number = {secondNumber}");
-
-            firstNumber = firstNumber + secondNumber;
-            secondNumber = firstNumber - secondNumber;
-            firstNumber = firstNumber - secondNumber;
-
-            Console.WriteLine("After Swapping:-");
-            Console.WriteLine($"First Number = {firstNumber}  , Second Number = {secondNumber}");
+            };
+            Console.WriteLine("Sun    Mon   Tue   Wed   Thu   Fri   Sat");
+            for (int i = 0; i < calendar.Length; i++)
+            {
+                for (int j = 0; j < calendar[i].Length; j++)
+                {
+                    //using format method to align output
+                    Console.Write(String.Format("{0,-7}", calendar[i][j]));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
